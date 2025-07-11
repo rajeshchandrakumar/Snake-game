7d7ea5bfa22ea3f64ba4c6ff28048b5c134b89d3
rajeshchandrakumar <rajeshrajesh91648@gmail.com>
2025-07-11 20:30:56 +0530

    feat : Detect collision with wall

--- main.py
+++ main.py
@@ -20,15 +20,20 @@ screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
-
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
 
+    # Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
         scoreboard.increase_score()
+
+    # Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
 
     snake.move()
 screen.exitonclick()
